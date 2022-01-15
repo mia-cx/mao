07ac3a15d3c86c0e75085280acd2bb411a68f86c
Mia <mia@mia.cx>
2022-01-15 10:58:42 +0100

    remove compiled files added compile command on postinstall

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import fs from "fs";
-exports.__esModule = true;
-var configuration_1 = require("./api/configuration");
-var config = configuration_1["default"].fromFile("./config.json");
-console.log(config);
--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
-// import fs from "fs";
-
 import Configuration from "./api/configuration";
 
 const config = Configuration.fromFile("./config.json");
 
-console.log(config);
+const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === "ping") {
+        await interaction.reply("Pong!");
+    }
+});
+
+client.login(config.token);
